5f0e241b9806341575cd26cea20e3360e037a91d
Daniel Ezekiel <dbezekiel77@gmail.com>
2024-02-12 12:26:26 +0100

    chore: setup nextConfig for pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "/spaceclub-oau",
+  output: "export", // <=== enables static exports
+  reactStrictMode: true,
+};
 
 export default nextConfig;
+
